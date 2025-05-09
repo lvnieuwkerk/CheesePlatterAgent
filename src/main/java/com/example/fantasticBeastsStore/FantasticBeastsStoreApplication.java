56955c923c0ea7d161b09fd06b42f1268c3fc84a
lisettevn <lisette.vannieuwkerk@trifork.nl>
2025-05-09 16:14:20 +0200

    Initial commit

---
+++ src/main/java/com/example/fantasticBeastsStore/FantasticBeastsStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.fantasticBeastsStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasticBeastsStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasticBeastsStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fantasticBeastsStore/FantasticBeastsStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fantasticBeastsStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasticBeastsStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
